709c01ca9853881b424baa90d43483d21d5de74b
Paul Branson <paul.branson@uwa.edu.au>
2021-11-08 01:51:40 +0000

    Fine tuning

---
+++ intake_aodn/analysis.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Copyright (c) 2020 - 2021, CSIRO 
+#
+# All rights reserved.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+                
+def make_clim(da,time_res='month',**kwargs):
+    import numpy as np
+    if 'time_slice' in kwargs:
+        ct = da.sel(time=slice(kwargs['time_slice'][0],kwargs['time_slice'][1])).groupby('time.' + time_res).count(dim='time')
+        s = da.sel(time=slice(kwargs['time_slice'][0],kwargs['time_slice'][1])).groupby('time.' + time_res).std(dim='time')
+        clim = da.sel(time=slice(kwargs['time_slice'][0],kwargs['time_slice'][1])).groupby('time.' + time_res).mean(dim='time')
+    else:
+        clim = da.groupby('time.' + time_res).mean(dim='time')
+        s = da.groupby('time.' + time_res).std(dim='time')
+        ct = da.groupby('time.' + time_res).count(dim='time')
+        
+    h95 = clim + 1.96*s/np.sqrt(ct)
+    l95 = clim - 1.96*s/np.sqrt(ct)
+    return clim,h95,l95
+
+
+
+def time_average(da,dt,**kwargs):
+    import pandas as pd #
+    #dt specifies the type of resampling and can take values of 'M', 'Y' or 'DJF' for season
+    if len(dt)==3:
+        quarters = {'DJF':'Q-Feb','MAM':'Q-May','JJA':'Q-Aug','SON':'Q-Nov'}
+        mth_num = {'DJF':2,'MAM':5,'JJA':8,'SON':11}
+        m = mth_num[dt]
+        dt_q = quarters[dt]
+        t_unit = 'Y'
+        # Method 1: ignores incomplete seasons
+        if kwargs['ignore_inc']:
+            avg_da=da[kwargs['var']].resample(time=dt_q).mean(skipna = True)
+            avg_da = avg_da.sel(time=avg_da['time.month']==m).groupby('time.year').mean()
+        else:
+        # Method2: replaces incomplete seasons with Na
+            avg_da=da[kwargs['var']].resample(time='1M').mean()
+            avg_da = da.where(avg_da.time.dt.season==dt).rolling(min_periods=3,center=True,time=3).mean()
+            avg_da = avg_da.groupby('time.year').mean('time')[kwargs['var']]
+    else:
+        t_unit = dt
+        dt = '1'+dt
+        avg_da=da[kwargs['var']].resample(time=dt).mean(skipna=True)
+        avg_da['time'] = avg_da.time.astype('datetime64[' + t_unit +']')
+        if not kwargs['ignore_inc']:
+            ext_time = avg_da['time'][[0, len(avg_da['time'])-1]]
+            if (da.time[len(da.time)-1].dt.day.values < 15) or ((da.time[len(da.time)-1].dt.month.values < 12) and (t_unit=='Y')):
+                avg_da = avg_da.where(avg_da.time!=avg_da.time[len(avg_da.time)-1])
+
+            if avg_da['time'][0] < da['time'][0]-pd.to_timedelta(15,'D'):
+                avg_da = avg_da.where(avg_da.time!=avg_da.time[0])
+            #last_day = da.time[(da.time + pd.to_timedelta(1,'D')).dt.month != (da.time).dt.month].astype('datetime64[M]') 
+            #avg_da = avg_da.where((avg_da.time.isin(last_day) & avg_da.time.isin(da.time)))
+
+    return avg_da
+
+    
+def lin_trend(da,coord,deg=1):
+    import xarray as xr
+    import numpy as np
+    from scipy import stats
+    
+    f=da.polyfit(dim=coord,deg=1)
+    fit = xr.polyval(da[coord],f)
+    fit = fit.rename({'polyfit_coefficients':'linear_fit'})
+    n = len(da[coord])
+    x2 = xr.DataArray(range(1,len(da[coord])+1),dims =coord,coords={coord:da[coord]})
+    serr= np.sqrt(((da-fit['linear_fit'])**2).sum(dim=coord)/(len(da[coord])-1)).expand_dims(dim = {coord:n})
+    t = stats.t.ppf(1-0.025, len(da[coord]))
+    B = np.sqrt(1/n + (x2 - np.mean(x2))**2 / np.sum((x2-np.mean(x2))**2))
+    ci = B*serr*t
+    ci+fit
+
+    hci = ci+fit
+    lci = fit-ci
+   # f=da.polyfit(dim=coord,deg=1)
+   # fit = xr.polyval(da[coord],f)
+   # fit = fit.rename({'polyfit_coefficients':'linear_fit'})
+   # n = len(da[coord])
+   # x2 = range(1,len(da[coord])+1)
+   # serr = np.sqrt(np.sum((da-fit)**2)/(len(da[coord])-1))
+   # t = stats.t.ppf(1-0.025, len(da[coord]))
+
+    #ci = t * serr['linear_fit'].values * np.sqrt(1/n + (x2 - np.mean(x2))**2 / np.sum((x2-np.mean(x2))**2))
+    #hci = ci+fit
+    #lci = fit-ci
+    return f,fit,hci,lci
+
--- intake_aodn/utils.py
+++ intake_aodn/utils.py
@@ -23,7 +23,7 @@ def get_distributed_cluster(worker_cores=1,worker_memory=2.0, min_workers=1, max
     from dask_gateway import Gateway
 
     if worker_cores > 4:
-        raise ValueError('Please dont create ')
+        raise ValueError('Please dont create a worker larger than 4 cores')
 
     # Dask gateway
     gateway = Gateway()
@@ -43,5 +43,61 @@ def get_distributed_cluster(worker_cores=1,worker_memory=2.0, min_workers=1, max
     display(cluster)
     return client
 
+def display_entry(entry):
+    import pandas as pd #
+    print(f'NAME: {entry.name}')
+    print(f'DESCRIPTION: {entry.description}')
+    print(f'TYPE: {entry.container}')
+    print(f'METADATA: ')
+    for k,v in entry.metadata.items():
+       print(f'    {k} : {v}') 
+    print('USER PARAMETERS:')
+    user_params = pd.DataFrame(entry.describe()['user_parameters'])
+    display(user_params)
+    
+def get_list_datasets(cat):
+    global da_list
+    da_list = []
+    global ser_list 
+    ser_list = []
+    da_ser = ["aodn_s3","nci"]
+    for ser in da_ser:
+        for entry in cat[ser]:
+            da_list.append(cat[ser][entry].name)
+            ser_list.append(ser)
+        
+    return da_list,ser_list
+
+def save_netcdf(data,filename):
+# Need to drop the "NAME" and "_Netcdf4Dimid" which are reserved when writing back out to netCDF
+# Unclear why they are in the dataset
+            
+    # Unclear why they are in the dataset
+    for v in data.variables:
+        for bad_attr in ['_Netcdf4Dimid','NAME']:
+            if bad_attr in data[v].attrs.keys():
+                del data[v].attrs[bad_attr]
+
+    data.to_netcdf(filename)
+ 
+
+def save_excel(df_list,file_name = "myfile",*args):
+# args can contain a list of the name of sheets (sheetname)
+# large spatial daily files can take 10-20mins
+    if 'sheetname' in args:
+        sheetname = args["sheet_name"]
+    else:
+        sheetname = list(str(range(1,len(df_list)+1)))
+        
+    with pd.ExcelWriter(file_name+'.xlsx') as writer:
+    
+        for i in range(0,len(df_list)):
+            if df_list[i].shape[0]>=2**20:
+                print("Warning: data #" +str(i)+ " is too large to save as a .xlsx file")
+            else:
+                df_list[i].to_excel(writer,sheet_name=str(sheetname[i]))
+
+
+                
 
 
