d9eeb0009430423b4c2a5737948153dbe61ef132
Paul Branson <paul.branson@uwa.edu.au>
2022-02-21 15:17:10 +0000

    Add versioneer, tidy setup.py

--- setup.py
+++ setup.py
@@ -7,42 +7,32 @@
 # The full license is in the LICENSE file, distributed with this software.
 #-----------------------------------------------------------------------------
 
-from setuptools import setup, find_packages
-
-requires = [line.strip() for line in open('requirements.txt').readlines()
-            if not line.startswith("#")]
+from os.path import exists
+from setuptools import setup
+import versioneer
 
 setup(
     name='intake-aodn',
-    version='0.0.1a'
+    version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='Relocatable Ocean Modelling in PYthon (rompy)',
+    description='A collection of intake catalogs and drivers to access AODN data directly in AWS S3',
     url='https://github.com/IOMRC/intake-aodn',
     maintainer='Paul Branson',
     maintainer_email='paul.branson@csiro.au',
     license='BSD',
     py_modules=['intake_aodn'],
-    package_data={'': ['*.csv', '*.yml', '*.yaml', '*.html']},
-    include_package_data=True,
-    install_requires=requires,
-    packages=find_packages(),
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
+    packages=['intake_aodn',],
     entry_points={
         'intake.drivers': [
-            'refzarr_stack = intake_aodn.driver:NetCDFFCStackSource'
+            'refzarr_stack = intake_aodn.drivers:RefZarrStackSource'
         ],
         'intake.catalogs': [
             'aodn_data = intake_aodn:cat'
         ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    python_requires=">=3.6",
-    long_description=open('README.md').read(),
+    python_requires=">=3.7",
+    long_description=(open('README.md').read() if exists('README.md') else ''),
     long_description_content_type="text/markdown",
-    zip_safe=False,
+    zip_safe=True,
 )
